af4161a180ad92ce375c764e04dc8d1b0145de4c
Ivan Ushachov <ushachovg324@gmail.com>
2025-09-17 21:51:33 +0300

    update CI/CD

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
-import path from 'path';
-
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
+      '@': resolve(__dirname, 'src'),
+    }
   },
+  build: {
+    outDir: 'dist'
+  }
 });
